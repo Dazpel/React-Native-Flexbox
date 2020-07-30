20c75501cfae21ab3bf94a563755468ad8f5289b
Dazpel <alexxdav97@gmail.com>
2020-07-29 21:25:55 -0400

    init

---
+++ components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = (props) => {
+
+    console.log(props.style)
+
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: '#fff',
+    elevation: 5,
+    padding: 20,
+    borderRadius: 10,
+  },
+});
+
+export default Card
